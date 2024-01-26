44e26fbdfc611d2707e6f8acb3a65fcb42905a0d
AE06909 <wes.gonzales@elevancehealth.com>
2024-01-26 09:18:11 -0500

    Finishing up exploration of Hooks.

---
+++ chapter3-use-callback/src/App.js
@@ -0,0 +1,128 @@
+// import React, { useState, useCallback } from "react"
+// import { v4 as uuid } from "uuid"
+
+// const App = () => {
+
+//     console.log("Rendering process for: App Component")
+
+//     const [speakers, setSpeakers] = useState([
+//         { id: "1", name: "Brown Lewis" },
+//         { id: "2", name: "Micheal John" },
+//         { id: "3", name: "Donald Bolton" },
+//         { id: "4", name: "Ambrov Lee" },
+//     ]);
+
+//     const [text, setText] = useState("")
+
+//     const handleTextInput = (e) => {
+//         setText(e.target.value)
+//     }
+
+//     const handleAddSpeaker = () => {
+//         setSpeakers(speakers.concat({
+//             id: uuid(),
+//             name: text
+//         }))
+//     }
+
+//     // useCallback, the code is similar to useMemo except that useCallback
+//     // is wrapped around the function we intend to cache or memoize.
+//     const handleRemoveSpeaker = useCallback(
+//         (id) => setSpeakers(speakers.filter((user) => user.id !== id)), [speakers]
+//     )
+
+//     return (
+//         <div>
+//             <input type="text" value={text} onChange={handleTextInput} />
+//             <button type="button" onClick={handleAddSpeaker}>+ Add a Speaker</button>
+
+//             <List list={speakers} onRemove={handleRemoveSpeaker} />
+//         </div>
+//     )
+// }
+
+// const List = React.memo(({ list, onRemove }) => {
+//     console.log("Rendering process for: List Component")
+//     return (
+//         <ul>
+//             {list.map((item) => (
+//                 <ListItem key={item.id} item={item} onRemove={onRemove} />
+//             ))}
+//         </ul>
+//     )
+// })
+
+// const ListItem = React.memo(({ item, onRemove }) => {
+//     console.log("Rendering process for: ListItem Component")
+//     return (
+//         <li>
+//             {item.name}
+//             <button type="button" onClick={() => onRemove(item.id)}>Remove</button>
+//         </li>
+//     )
+// })
+
+
+
+// export default App;
+import React, { useState, useCallback } from "react";
+import { v4 as uuid } from "uuid";
+
+const App = () => {
+    console.log("Rendering process for: App Component");
+
+    const [speakers, setSpeakers] = useState([
+        { id: "1", name: "Brown Lewis" },
+        { id: "2", name: "Micheal John" },
+        { id: "3", name: "Donald Bolton" },
+        { id: "4", name: "Ambrov Lee" },
+    ]);
+
+    const [text, setText] = useState("");
+
+    const handleTextInput = (e) => {
+        setText(e.target.value);
+    };
+
+    const handleAddSpeaker = () => {
+        setSpeakers(speakers.concat({
+            id: uuid(),
+            name: text
+        }));
+    };
+
+    const handleRemoveSpeaker = useCallback(
+        (id) => setSpeakers((prevSpeakers) => prevSpeakers.filter((user) => user.id !== id)), []
+    );
+
+    return (
+        <div>
+            <input type="text" value={text} onChange={handleTextInput} />
+            <button type="button" onClick={handleAddSpeaker}>+ Add a Speaker</button>
+            <List list={speakers} onRemove={handleRemoveSpeaker} />
+        </div>
+    );
+};
+
+const List = React.memo(({ list, onRemove }) => {
+    console.log("Rendering process for: List Component");
+    return (
+        <ul>
+            {list.map((item) => (
+                <ListItem key={item.id} item={item} onRemove={onRemove} />
+            ))}
+        </ul>
+    );
+});
+
+const ListItem = React.memo(({ item, onRemove }) => {
+    console.log("Rendering process for: ListItem Component");
+    return (
+        <li>
+            {item.name}
+            <button type="button" onClick={() => onRemove(item.id)}>Remove</button>
+        </li>
+    );
+});
+
+export default App;
--- chapter3-use-memo/src/App.js
+++ chapter3-use-memo/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useMemo, useRef } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Hypothetical data source.
+const speakers = [
+    {
+        id: 10,
+        name: "John Lewis"
+    },
+    {
+        id: 11,
+        name: "Marble Newton"
+    }
+]
+
+const App = () => {
+    // Variables to state.
+    // text and searchTerm are decalred with their setter methods
+    // as variables.
+    // const [text, setText] = useState("")
+    const [searchTerm, setSearchTerm] = useState("")
+    const inputRef = useRef()
+
+    // const onChangeText = (e) => {
+    //     setText(e.target.value)
+    // }
+
+    // console.log("Text", text)
+
+    // Event handler to update the initial state to the current state.
+    const handleClick = () => {
+        setSearchTerm(inputRef.current.value)
+    }
+
+    // console.log("Search Term", text)
+
+    /*
+        Used to filter an array of speakers based on the searchTerm using
+       case insensitive search.
+    */
+    // const filteredSpeakers = speakers.filter((speaker) => {
+    //     console.log("Filtering speakers...")
+    //     return speaker.name.toLowerCase().includes(searchTerm.toLowerCase())
+    // })
+    const filteredSpeakers = useMemo(() => speakers.filter((speaker) => {
+        console.log("Filtering speakers...");
+        return speaker.name.toLowerCase()
+            .includes(searchTerm.toLowerCase());
+    }), [searchTerm]);
+
+
+    return (
+        <div>
+            <div>
+                <input ref={inputRef} type="text" />
+                <button onClick={handleClick}>Search</button>
+            </div>
+            {filteredSpeakers.map((filteredSpeaker) => (
+                <li key={filteredSpeaker.id}>{filteredSpeaker.name}</li>
+            ))}
+        </div>
+    )
 }
 
 export default App;
