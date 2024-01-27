fd76958201896e6ac5fa241aa148bf8e480bad86
AE06909 <wes.gonzales@elevancehealth.com>
2024-01-27 09:58:08 -0500

    Testing async/await and axios.

--- chapter4-async-await/src/App.js
+++ chapter4-async-await/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [data, setData] = useState([])    // Initialize state as an empty array.
+
+    useEffect(() => {
+        const API_URL = "https://dummyjson.com/users"
+
+        const fetchSpeakers = async () => {
+            try {
+                const response = await fetch(API_URL)
+                const data = await response.json()
+
+                setData(data.users)
+            } catch (error) {
+                console.log("error", error)
+            }
+        }
+        fetchSpeakers()
+    }, [])
+
+    return (
+        <>
+            <ul>
+                {data.map(item => (
+                    <li key={item.id}>
+                        {item.firstName} {item.lastName}
+                    </li>
+                ))}
+            </ul>
+        </>
+    )
 }
 
 export default App;
