34c949c701e7d2faee7774c69ff6688b38ffd2ba
AE06909 <wes.gonzales@elevancehealth.com>
2024-01-27 10:08:55 -0500

    Test react query and axios.

--- chapter4-axios/src/App.js
+++ chapter4-axios/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react"
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [data, setData] = useState([])
+
+    // getSpeakers uses axios.get() to fetch data from the endpoint
+    // and returns a promise.
+    const getSpeakers = () => {
+        axios.get("https://jsonplaceholder.typicode.com/users")
+            .then(response => {
+                setData(response.data)
+            })
+    }
+
+    useEffect(() => {
+        getSpeakers()
+    }, [])
+
+
+    return (
+        <>
+            <h1>Displaying Speakers Information</h1>
+            <ul>
+                {data.map(speaker => (
+                    <li key={speaker.id}>
+                        {speaker.name}, <em>{speaker.email}</em>
+                    </li>
+                ))}
+            </ul>
+        </>
+    )
 }
 
 export default App;
