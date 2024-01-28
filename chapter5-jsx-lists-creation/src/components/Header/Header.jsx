d5ba8dcd995cecf4f168c320b8d6eb9fc877162a
AE06909 <wes.gonzales@elevancehealth.com>
2024-01-28 08:35:51 -0500

    List components.

---
+++ chapter5-jsx-lists-creation/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+
+    return (
+
+        <header className="header">
+            <div className="logo">Bizza</div>
+            <nav className="nav">
+                <ul>
+                    <li><Link to="/" className='navLink'>Home</Link></li>
+                    <li><Link to="/speakers" className='active navlink'>Speakers</Link></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
---
+++ chapter5-jsx-lists-creation/src/pages/SpeakersPage/SpeakersPage.js
@@ -0,0 +1,35 @@
+import React from "react"
+import SpeakersCard from "../../components/SpeakerCard/SpeakerCard"
+import speakerList from "../../data/SpeakerList"
+import Breadcrumb from "../../components/Common/Breadcrumb/Breadcrumb"
+import Header from "../../components/Header/Header"
+import Footer from "../../components/Footer/Footer"
+
+const SpeakersPage = () => {
+
+    return (
+        <>
+            <Header />
+            <Breadcrumb title={"Speakers"} />
+            <div className="speakers-container">
+                <div className="section-heading">
+                    <h1>Meet Our Speakers</h1>
+                </div>
+                <div className="card">
+                    {speakerList.map((speaker) => (
+                        <SpeakersCard
+                            key={speaker.id}
+                            name={speaker.name}
+                            jobTitle={speaker.jobTitle}
+                            company={speaker.company}
+                            profileImg={speaker.profileImg}
+                        />
+                    ))}
+                </div>
+            </div>
+            <Footer />
+        </>
+    )
+}
+
+export default SpeakersPage
