4557a1aff3171fb42c074692f409eb0b903f00ce
AE06909 <wes.gonzales@elevancehealth.com>
2024-01-30 07:24:43 -0500

    Created project for Jest testing.

---
+++ chapter7-testing/__tests__/basicFunctions.test.js
@@ -0,0 +1,5 @@
+const basicFunctions = require('../basicFunctions')
+
+test('Multiply 9 * 5 to equal 45', () => {
+    expect(basicFunctions.multiply(9, 5)).toBe(45)
+})
---
+++ chapter7-testing/basicFunctions.js
@@ -0,0 +1,7 @@
+const basicFunctions = {
+
+    multiply: (num1, num2) => num1 * num2
+
+}
+
+module.exports = basicFunctions
